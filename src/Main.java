938d94e7be772eac2b3f0eae75355fdc73d42f6a
Mi5o <1847638968@qq.com>
2026-06-07 20:50:29 +0800

    基础菜单界面修改

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,7 @@ public class Main {
         System.out.println("请选择操作：");
         System.out.println("1.读取文档文件");
         System.out.println("2.基础格式排版（默认格式、冗余清理）");
-        System.out.println("3.");
-        System.out.println("4.保存排版后文档");
-        System.out.println("5.退出系统");
+        System.out.println("3.保存排版后文档");
+        System.out.println("4.退出系统");
     }
 }
