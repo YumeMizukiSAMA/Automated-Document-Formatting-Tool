ba733e21f2c35036603c80e0d6c988915a18bfca
Mi5o <1847638968@qq.com>
2026-06-08 19:37:59 +0800

    commit 5：修正调用问题

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         boolean running = true;
+        DocumentFormatter formatter = new DocumentFormatter();
         MenuController.showMenu();
         while (running){
             switch (MenuController.handleChoice()){
-                case 1 -> loadDocument();
-                case 2 -> formatDocument();
-                case 3 -> saveDocument();
+                case 1 -> formatter.loadDocument();
+                case 2 -> formatter.formatDocument();
+                case 3 -> formatter.saveDocument();
             }
         }
     }
